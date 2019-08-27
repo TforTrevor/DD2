8846de8bef5feef94e4f3f9f65b44e173665269e
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2019-08-27 15:45:49 -0400

    Keep Distance toggle for move to target

--- Assets/DD2/Scripts/AI/Actions/MoveToTarget.cs
+++ Assets/DD2/Scripts/AI/Actions/MoveToTarget.cs
@@ -7,12 +7,17 @@ namespace DD2.AI.Actions
     [CreateAssetMenu(menuName = "Scriptable Objects/AI/Actions/Move To Target")]
     public class MoveToTarget : Action
     {
+        [SerializeField] bool ensureMaxRange;
         public override void Act(StateController controller)
         {
-            Vector3 direction = Vector3.Normalize(controller.status.target.position - controller.transform.position);
-            float distance = Vector3.Distance(controller.status.target.position, controller.transform.position) - controller.status.stats.GetAttackRange();
-            Vector3 position = (direction * distance) + controller.transform.position;
-            controller.status.navMeshAgent.SetDestination(position);
+            float distance = Vector3.Distance(controller.status.target.position, controller.transform.position);
+            if (distance > controller.status.stats.GetAttackRange() || ensureMaxRange)
+            {
+                Vector3 direction = Vector3.Normalize(controller.status.target.position - controller.transform.position);
+                float distanceFromRange = distance - controller.status.stats.GetAttackRange();
+                Vector3 position = (direction * distanceFromRange) + controller.transform.position;
+                controller.status.navMeshAgent.SetDestination(position);
+            }
         }
     }
 }
