e879ef98e050477216f387e9b58e78e6f8e0c84b
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2019-09-07 22:32:37 -0400

    Basic object pooling

--- Assets/DD2/Scripts/Ability/TargetAbility.cs
+++ Assets/DD2/Scripts/Ability/TargetAbility.cs
@@ -7,6 +7,14 @@ namespace DD2.Abilities
 {
     public class TargetAbility : Ability
     {
+        [SerializeField] string objectKey;
+        ObjectPool objectPool;
+
+        void Awake()
+        {
+            objectPool = GetComponent<ObjectPool>();
+        }
+
         public override void UseAbility(Transform target)
         {
             if (onCooldown)
@@ -14,6 +22,16 @@ namespace DD2.Abilities
                 return;
             }
 
+            GameObject vfx = objectPool.GetObject(objectKey);
+            if (vfx != null)
+            {
+                vfx.transform.position = transform.position;
+                Projectile projectile = vfx.GetComponent<Projectile>();
+                projectile.target = target;
+                projectile.objectPool = objectPool;
+                vfx.SetActive(true);
+            }
+            
             for (int i = 0; i < abilityEffects.Length; i++)
             {
                 abilityEffects[i].ApplyEffect(this, target);
---
+++ Assets/DD2/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD2
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float speed;
+        [SerializeField] float destroyDistance;
+        public Transform target;
+        [HideInInspector] public ObjectPool objectPool;
+
+        void Update()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            Vector3 direction = Vector3.Normalize(target.position - transform.position);
+            transform.position += direction * speed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, target.position) < destroyDistance)
+            {
+                objectPool.ReturnObject("Projectile", this.gameObject);
+            }
+        }
+    }
+}
