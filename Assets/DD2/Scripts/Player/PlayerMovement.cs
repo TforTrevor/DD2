af7a03b4ae0b497513bb4d02fd474a0e8cc4da13
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2020-12-10 02:40:15 -0500

    update movement

--- Assets/DD2/Scripts/Player/PlayerMovement.cs
+++ Assets/DD2/Scripts/Player/PlayerMovement.cs
@@ -21,7 +21,6 @@ namespace DD2
         [SerializeField] LayerMask groundedMask;
         [SerializeField] float groundedOffset;
         [SerializeField] float groundedLength;
-        [SerializeField] float groundedRadius;
 
         Rigidbody rb;
         int currentJumps;
@@ -30,6 +29,7 @@ namespace DD2
         public bool IsMoving { get; private set; }
         public bool IsGrounded { get; private set; }
         public Vector3 Velocity { get; private set; }
+        public float VerticalVelocity { get; private set; }
 
         void Awake()
         {
@@ -46,18 +46,21 @@ namespace DD2
             {
                 Move();
             }
-            rb.AddForce(Vector3.down * 9.81f);
+
+            if (!IsGrounded)
+            {
+                VerticalVelocity += Physics.gravity.y * Time.deltaTime;
+            }
 
             RaycastHit hit;
-            if (Physics.SphereCast(transform.position + new Vector3(0, groundedOffset, 0), groundedRadius, Vector3.down, out hit, groundedLength, groundedMask)) 
-            {
-                IsGrounded = true;
-                currentJumps = 0;
-            }
-            else
+            bool hitGround = Physics.Raycast(transform.position + new Vector3(0, groundedOffset, 0), Vector3.down, out hit, groundedLength, groundedMask);
+            if (!hitGround)
             {
                 IsGrounded = false;
             }
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(Velocity, hit.normal);
+            rb.velocity = new Vector3(projectedVelocity.x, projectedVelocity.y + VerticalVelocity, projectedVelocity.z);
         }
 
         void Move()
@@ -66,29 +69,41 @@ namespace DD2
             {
                 IsMoving = true;
 
-                float tempAcceleration = IsGrounded ? acceleration : airAcceleration;
-                Velocity += transform.TransformVector(new Vector3(moveInput.Value.x * tempAcceleration * Time.deltaTime, 0, moveInput.Value.y * tempAcceleration * Time.deltaTime));
-                Velocity = Vector3.ClampMagnitude(Velocity, stats.MoveSpeed);
+                float tempSpeed = stats.MoveSpeed;
+                float tempAcceleration = acceleration;
+                if (!IsGrounded)
+                {
+                    tempAcceleration = airAcceleration;
+                    tempSpeed = Mathf.Max(Velocity.magnitude, tempSpeed);
+                }
+
+                Vector3 inputVector = new Vector3(moveInput.Value.x, 0, moveInput.Value.y).normalized;
+
+                Vector3 targetVelocity = transform.TransformDirection(inputVector) * tempSpeed;
+                Vector3 force = (targetVelocity - Velocity).normalized * tempAcceleration;
+                Velocity += force * Time.deltaTime;
             }
             else
             {
                 IsMoving = false;
 
-                if (IsGrounded && Velocity.magnitude > 0)
+                if (IsGrounded)
                 {
-                    Velocity -= Velocity.normalized * brakeSpeed * Time.deltaTime;
+                    if (Velocity.magnitude > 0.25f)
+                    {
+                        Velocity -= Velocity.normalized * brakeSpeed * Time.deltaTime;
+                    }
+                    else
+                    {
+                        Velocity = Vector3.zero;
+                    }
                 }
             }
-
-            rb.MovePosition(rb.position + Velocity * Time.deltaTime);
         }
 
         void Rotate()
         {
-            Quaternion deltaRotation = Quaternion.AngleAxis(lookInput.Value.x * sensitivity * Time.deltaTime, Vector3.up);
-            rb.MoveRotation(rb.rotation * deltaRotation);
-
-            //transform.Rotate(Vector3.up, lookInput.Value.x * sensitivity * Time.deltaTime);
+            transform.Rotate(Vector3.up, lookInput.Value.x * sensitivity * 0.01f * Time.timeScale);
         }
 
         public void Jump(bool value)
@@ -96,7 +111,7 @@ namespace DD2
             //Only call when jump is pressed down
             if (IsGrounded && value && currentJumps < maximumJumps)
             {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                VerticalVelocity += jumpForce;
                 currentJumps++;
             }
         }
@@ -104,6 +119,30 @@ namespace DD2
         public void AddForce(Vector3 force, ForceMode forceMode)
         {
             rb.AddForce(force, forceMode);
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (Util.Utilities.IsInLayer(collision.collider.gameObject, groundedMask))
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position + new Vector3(0, groundedOffset, 0), Vector3.down, out hit, groundedLength, groundedMask))
+                {
+                    IsGrounded = true;
+                    currentJumps = 0;
+                    VerticalVelocity = 0;
+                }
+            }
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            RaycastHit hit;
+            bool hitGround = Physics.Raycast(transform.position + new Vector3(0, 0, 0), Vector3.down, out hit, 0.1f, groundedMask);
+            if (Util.Utilities.IsInLayer(collision.collider.gameObject, groundedMask) && !hitGround)
+            {
+                IsGrounded = false;
+            }
         }
     }
 }
